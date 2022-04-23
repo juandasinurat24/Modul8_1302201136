77a900624ac940d175947fa5ca414aa3e9b750bc
JUANDA SINURAT <juandasinurat@365.telkomuniversity.ac.id>
2022-04-23 17:15:18 +0700

    menambahkan API Movies

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -7,5 +7,37 @@ namespace Modul8_1302201136.Controllers
     [ApiController]
     public class MovieController : ControllerBase
     {
-    }
+		// Mendapatkan semua movie yang ada
+		[HttpGet]
+		public ActionResult<List<Movie>> GetMovies()
+		{
+			return Movie.Movies.ToList();
+		}
+
+		// Menambahkan sebuah movie
+		[HttpPost]
+		public ActionResult<Movie> AddMovie(Movie movie)
+		{
+			Movie.Movies.Add(movie);
+			return Ok("Movie has been added");
+		}
+
+		// Mendapatkan satu objek movie
+		[HttpGet]
+		[Route("{id}")]
+		public ActionResult<Movie> GetMovie(int id)
+		{
+			Movie movie = Movie.Movies[id];
+			return Ok(movie);
+		}
+
+		// Menghapus sebuah movie dari database
+		[HttpDelete]
+		[Route("{id}")]
+		public ActionResult DeleteMovie(int id)
+		{
+			Movie.Movies.Remove(Movie.Movies[id]);
+			return Ok("Successfully delete movie");
+		}
+	}
 }
